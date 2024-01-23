a049444ff20858b5800259b709cc812964acf163
LucasGlave <lucasglave7@gmail.com>
2024-01-22 21:14:09 -0300

    feat(components): estructure

---
+++ components/HomeGestionarPaquete.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeGestionarPaquete = () => {
+  return (
+    <View>
+      <Text>HomeGestionarPaquete</Text>
+    </View>
+  )
+}
+
+export default HomeGestionarPaquete
