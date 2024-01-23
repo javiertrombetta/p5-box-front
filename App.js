38a2cbaf339b47369806b7711c853c64495f51b7
foxxenflay <boristenduro09@gmail.com>
2024-01-22 22:58:04 -0300

    feat(nativewind): install nativewind

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-400">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
