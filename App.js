f34a67934d5a171bb8524e08053341132daac6e3
javiertrombetta <jitrombetta@hotmail.com>
2024-01-25 10:36:33 -0300

    feat(tsc): add Typcescript to project

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { NativeWindStyleSheet } from "nativewind";
-import HomeGestionarPedido from "./src/components/HomeGestionarPedido";
-import PerfilRepartidor from "./src/components/PerfilRepartidor";
-import Repartidores from "./src/components/Repartidores";
-import Login from "./src/components/Login";
-import Paquetes from "./src/components/Paquetes";
-import AddPackage from "./src/components/AddPackage";
+import HomeGestionarPedido from "./src/screens/HomeGestionarPedido";
+import PerfilRepartidor from "./src/screens/PerfilRepartidor";
+import Repartidores from "./src/screens/Repartidores";
+import Login from "./src/screens/Login";
+import Paquetes from "./src/screens/Paquetes";
+import AddPackage from "./src/screens/AddPackage";
 import { useFonts } from "expo-font";
 
 NativeWindStyleSheet.setOutput({
@@ -17,8 +17,7 @@ export default function App() {
   const [loaded] = useFonts({
     SairaRegular: require("./assets/fonts/Saira-Regular.ttf"),
     SairaBold: require("./assets/fonts/Saira-Bold.ttf"),
-    RobotoRegular: require("./assets/fonts/Roboto-Regular.ttf"),
-    RobotoRegular: require("./assets/fonts/Roboto-Regular.ttf"),
+    RobotoRegular: require("./assets/fonts/Roboto-Regular.ttf"),    
     RobotoLight: require("./assets/fonts/Roboto-Light.ttf"),
     RobotoBold: require("./assets/fonts/Roboto-Bold.ttf"),
   });
